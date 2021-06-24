ca8a18f1571d92478647edfcf35601861a138376
Gadi <62388856+Gadiler@users.noreply.github.com>
2021-06-24 23:14:08 +0300

    changes

--- server.js
+++ server.js
@@ -5,11 +5,13 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/stam-gadi'));
+app.use(express.static('./dist/stam'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/stam-gadi/'}),
+    res.sendFile('index.html', {root: 'dist/stam/'}),
 );
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
+
+express().listen(PORT, () => console.log(`Listening on ${ PORT }`))
